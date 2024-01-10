931b0f45c2bd54f571ad7cf030ac0768cd101edf
Hasnain <ansarihasnain07@gmail.com>
2024-01-10 15:36:24 +0300

    shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      // home: SplashScreen(),
 
-      // home: LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
